125a70841d90019958f3f5741c4286a9e06317e5
Aleem Isiaka <aleemisiaka@gmail.com>
2025-03-25 07:23:35 +0100

    adds more aliases to zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,11 +4,21 @@ alias python=python3
 alias work='cd $HOME/Work'
 alias config='cd $HOME/Work/configs'
 
+# Portfolio directory work related aliases
+alias blog='cd $HOME/Work/portfolio/limsitah.dev'
+
 # MC2 Work Related aliases
 alias mc2='cd $HOME/Work/mcsquared/'
 alias mc2m='cd $HOME/Work/mcsquared/mc2.mono/'
 alias mc2s='cd $HOME/Work/mcsquared/mc2.service-mono/'
 
+# Open Source related aliases
+alias oss='cd $HOME/Work/oss'
+alias obsp='cd $HOME/Work/obsp'
+
 # editor related aliases
 alias edcfg='nvim $HOME/Work/configs/dotfiles'
+alias edblog='nvim $HOME/Work/portfolio/limsitah.dev'
 
+# command related aliases
+alias lg='lazygit'
