0bbf429ff424da8cf45b0d42d1245aa5fd53640c
Aleem Isiaka <aleemisiaka@gmail.com>
2025-03-25 07:12:55 +0100

    minor aliases for work and configuration editor management

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,14 @@
 alias python=python3
+
+# work directory related aliases
+alias work='cd $HOME/Work'
+alias config='cd $HOME/Work/configs'
+
+# MC2 Work Related aliases
+alias mc2='cd $HOME/Work/mcsquared/'
+alias mc2m='cd $HOME/Work/mcsquared/mc2.mono/'
+alias mc2s='cd $HOME/Work/mcsquared/mc2.service-mono/'
+
+# editor related aliases
+alias edcfg='nvim $HOME/Work/configs/dotfiles'
+
