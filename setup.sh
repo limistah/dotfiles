11946eb4250b5b583cdbb342002ffbf3af2612a0
Aleem Isiaka <aleemisiaka@Mac.gtaxpro.local>
2025-07-22 18:53:28 +0100

    setup more dotfiles for automation

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
+if ! command -v stow >/dev/null 2>&1; then
+    echo "stow not found. Installing..."
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get update && sudo apt-get install -y stow
+    elif [ -x "$(command -v brew)" ]; then
+        brew install stow
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf install -y stow
+    elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman -Sy --noconfirm stow
+    else
+        echo "Could not detect package manager. Please install 'stow' manually."
+        exit 1
+    fi
+fi
 
 stow .
