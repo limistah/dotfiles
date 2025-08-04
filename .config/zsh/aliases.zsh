e4f47dd7d6b40af0ae11f0dfe37165dc27aab3b7
Aleem Isiaka <aleemisiaka@gmail.com>
2025-08-04 14:21:17 +0100

    installed oh-my-zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,24 +1,29 @@
 alias python=python3
 
 # work directory related aliases
-alias work='cd $HOME/Work'
-alias config='cd $HOME/Work/configs'
+alias work='cd $HOME/Library/Work'
+alias config='cd $HOME/Library/Work/configs'
 
 # Portfolio directory work related aliases
-alias blog='cd $HOME/Work/portfolio/limsitah.dev'
+alias blog='cd $HOME/Library/Work/portfolio/limsitah.dev'
 
 # MC2 Work Related aliases
-alias mc2='cd $HOME/Work/mcsquared/'
-alias mc2m='cd $HOME/Work/mcsquared/mc2.mono/'
-alias mc2s='cd $HOME/Work/mcsquared/mc2.service-mono/'
+alias mc2='cd $HOME/Library/Work/mcsquared/'
+alias mc2m='cd $HOME/Library/Work/mcsquared/mc2.mono/'
+alias mc2s='cd $HOME/Library/Work/mcsquared/mc2.service-mono/'
 
 # Open Source related aliases
-alias oss='cd $HOME/Work/oss'
-alias obsp='cd $HOME/Work/obsp'
+alias oss='cd $HOME/Library/Work/oss'
+alias obsp='cd $HOME/Library/Work/obsp'
 
 # editor related aliases
-alias edcfg='nvim $HOME/Work/configs/dotfiles'
-alias edblog='nvim $HOME/Work/portfolio/limsitah.dev'
+alias edcfg='nvim $HOME/Library/Work/configs/dotfiles'
+alias edblog='nvim $HOME/Library/Work/portfolio/limsitah.dev'
 
 # command related aliases
 alias lg='lazygit'
+function gacp() {
+    git add .
+    git commit -m "$1"
+    git push
+}
