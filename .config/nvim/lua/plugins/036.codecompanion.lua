fd70dd2ce2d92fa528e80ba839f1c198b6b357f4
Aleem Isiaka <aleemisiaka@gmail.com>
2025-10-07 12:25:06 +0100

    feat(mcphub): add plugin config and update CodeCompanion integration
    - Add new mcphub.nvim plugin configuration in plugins/042.mcphub.lua
    - Remove obsolete servers.json from mcphub config
    - Update CodeCompanion plugin to depend on mcphub.nvim, telescope.nvim,
    and nvim-treesitter
    - Enhance CodeCompanion display options and integrate MCP tools via
    mcphub extension
    - Set mini.diff source to git for improved diff handling

--- .config/nvim/lua/plugins/036.codecompanion.lua
+++ .config/nvim/lua/plugins/036.codecompanion.lua
@@ -4,6 +4,8 @@ return {
 	cmd = "CodeCompanion",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim", -- Or your preferred picker
+		"nvim-treesitter/nvim-treesitter", -- Also a hard requirement
 		{
 			"ravitemer/mcphub.nvim",
 			build = "npm install -g mcp-hub@latest",
@@ -18,7 +20,7 @@ return {
 				local diff = require("mini.diff")
 				diff.setup({
 					-- Disabled by default
-					source = diff.gen_source.none(),
+					source = diff.gen_source.git(),
 				})
 			end,
 		},
@@ -57,6 +59,38 @@ return {
 						description = "Reject the suggested change",
 					},
 				},
+			},
+		},
+		display = {
+			chat = {
+				icons = {
+					chat_context = "📎️", -- You can also apply an icon to the fold
+					chat_fold = " ",
+				},
+				fold_context = true,
+				fold_reasoning = true,
+				intro_message = "Welcome to CodeCompanion ✨! Press ? for options",
+				separator = "─", -- The separator between the different messages in the chat buffer
+				show_context = true, -- Show context (from slash commands and variables) in the chat buffer?
+				show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+				show_settings = false, -- Show LLM settings at the top of the chat buffer?
+				show_token_count = true, -- Show the token count for each response?
+				show_tools_processing = true, -- Show the loading message when tools are being executed?
+				start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+
+				adapter = "copilot",
+				tools = {
+					["mcp"] = {
+						callback = function()
+							return require("mcphub.extensions.codecompanion")
+						end,
+						description = "Call tools and resources from the MCP Servers",
+						opts = {
+							requires_approval = true,
+						},
+					},
+				},
+				inline = { adapter = "copilot" },
 			},
 		},
 	},
