f3533fb1c0268fbd73efb8ae85bde8afb9eda108
Aleem Isiaka <aleemisiaka@gmail.com>
2026-01-21 15:27:20 +0100

    Change shebang to zsh and enhance setup script with oh-my-zsh and tmux installation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 if ! command -v stow >/dev/null 2>&1; then
     echo "stow not found. Installing..."
     if [ -x "$(command -v apt-get)" ]; then
@@ -19,6 +19,37 @@ fi
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
 stow . --adopt
+
+source "$HOME/.config/zsh/init.zsh"
+
+# install oh-my-zsh if not installed
+if [ ! -d "$ZSH" ]; then
+    echo "Oh My Zsh not found. Installing..."
+    git clone https://github.com/ohmyzsh/ohmyzsh.git "$ZSH"
+    brew install zsh-completions
+    git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+    fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+    autoload -U compinit && compinit
+    source "$ZSH/oh-my-zsh.sh"
+fi
+
+# install tmux if not installed
+if ! command -v tmux >/dev/null 2>&1; then
+    echo "tmux not found. Installing..."
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get update && sudo apt-get install -y tmux
+    elif [ -x "$(command -v brew)" ]; then
+        brew install tmux
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf install -y tmux
+    elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman -Sy --noconfirm tmux
+    else
+        echo "Could not detect package manager. Please install 'tmux' manually."
+        exit 1
+    fi
+    git clone https://github.com/jimeh/tmuxifier.git $HOME/.tmuxifier
+fi
 
 
 # still, some new things can still happen here
