01d7a522fd09e2c911584153a819dc0a0e63a789
Aleem Isiaka <aleemisiaka@gmail.com>
2026-01-21 15:28:52 +0100

    Add installation check for nvim in setup script

--- setup.sh
+++ setup.sh
@@ -15,6 +15,23 @@ if ! command -v stow >/dev/null 2>&1; then
     fi
 fi
 
+# install nvim
+if ! command -v nvim >/dev/null 2>&1; then
+    echo "nvim not found. Installing..."
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get update && sudo apt-get install -y neovim
+    elif [ -x "$(command -v brew)" ]; then
+        brew install neovim
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf install -y neovim
+    elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman -Sy --noconfirm neovim
+    else
+        echo "Could not detect package manager. Please install 'nvim' manually."
+        exit 1
+    fi
+fi
+
 
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
