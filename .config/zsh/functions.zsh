58a818bfd060390d7999b07f36fc7c94fd43c24b
Aleem Isiaka <aisiaka@kredete.com>
2026-06-11 14:43:03 +0100

    update more

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -11,7 +11,7 @@ function swh() {
     local session="${dir##*/}"
     local layout="$HOME/.tmuxifier/layouts/${session}.session.sh"
     local template
-    
+
     if [[ "$dir" == *"/Work/kredete"* ]]; then
         template="$HOME/.config/tmuxifier/kredete.session.sh"
     else
@@ -26,3 +26,20 @@ function swh() {
     # Load and start the session
     tmuxifier load-session "$session"
 }
+
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '\es' sesh-sessions
+bindkey -M vicmd '\es' sesh-sessions
+bindkey -M viins '\es' sesh-sessions
